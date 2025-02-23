65e6ebc82547e957a1b9b9eeccaab11d6e230b5b
xyan0152 <xyan0152@student.monash.edu>
2025-02-23 14:36:54 +0800

    rbac-authority

---
+++ PhotoAppApiUsers/src/main/java/com/appsblog/photoapp/api/users/data/AuthorityEntity.java
@@ -0,0 +1,61 @@
+package com.appsblog.photoapp.api.users.data;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="authorities")
+public class AuthorityEntity implements Serializable {
+
+	private static final long serialVersionUID = -1657419209667475268L;
+	
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	@Column(nullable=false, length=20)
+	private String name;
+	
+	@ManyToMany(mappedBy="authorities")
+	private Collection<RoleEntity> roles;
+	
+	public AuthorityEntity() {
+		
+	}
+	
+	public AuthorityEntity(String name) {
+		this.name = name;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Collection<RoleEntity> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Collection<RoleEntity> roles) {
+		this.roles = roles;
+	}
+
+}
